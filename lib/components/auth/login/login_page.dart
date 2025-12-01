e695ca6e5afdce533d330cd9bc15d77151681a91
mukt-SPEC <alaminsad6@gmail.com>
2025-12-01 22:36:42 +0100

    started the login page

--- lib/components/auth/login/login_page.dart
+++ lib/components/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -6,5 +7,30 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+}
+
+class _LoginForm extends ConsumerStatefulWidget {
+  const _LoginForm({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => __LoginFormState();
+}
+
+class __LoginFormState extends ConsumerState<_LoginForm> {
+  final _formkey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
